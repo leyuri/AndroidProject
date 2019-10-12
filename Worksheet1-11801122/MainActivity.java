c484f30dd3d11ccaf1591c8794602f5e7c6c42ba
Yuri Lee <leyuri97@gmail.com>
2019-10-12 19:39:58 +0900

    View class

---
+++ Worksheet1-11801122/MainActivity.java
@@ -0,0 +1,33 @@
+package org.yuri.week1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter=0;
+    private TextView showCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showCount = (TextView)findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++counter;
+        if (showCount != null)
+            showCount.setText(Integer.toString(counter));
+    }
+
+
+}
